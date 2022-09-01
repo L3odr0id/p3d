91d1c25445d399870238eab325abff8fe67be141
L3odr0id <leon.leonc@yandex.ru>
2022-09-01 12:57:55 +0300

    No std

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,9 @@ use ndarray::Array3;
 use crate::algo_grid::get_contour;
 use crate::contour::Rect;
 
+extern crate core;
+mod ffi_interface;
+
 mod polyline;
 mod contour;
 mod algo_grid;
@@ -33,6 +36,16 @@ pub enum AlgoType {
 
 #[derive(Debug)]
 pub struct P3DError {}
+
+// fn main(){
+//     let res = ffi_interface::calc_inner("./3dpass-1308.obj".to_string(), 6, 6, Some([1 as u8,2 as u8,3 as u8,4 as u8]));
+//     // let res = ffi_interface::calc_inner(6, 6, "./pir1 3.obj".to_string());
+//     let r = match res {
+//         Ok(h) => println!("{}", h),
+//         Err(_e) => println!("error"),
+//     };
+//     // println!("Hello, world!");
+// }
 
 
 #[allow(unused_variables)]
