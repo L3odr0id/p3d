99ac5fa6a9f1e6595dd399f402e5f420371cc617
L3odr0id <leon.leonc@yandex.ru>
2022-09-02 17:23:04 +0300

    read file

--- src/ffi_interface.rs
+++ src/ffi_interface.rs
@@ -2,20 +2,22 @@ use alloc::ffi::CString;
 use alloc::string::{ToString, String};
 
 use crate::{p3d_process, AlgoType, P3DError};
-use core::ffi::{c_uchar, c_short, c_char, c_int};
+use core::ffi::{c_uchar, c_short, c_char, c_int, CStr};
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 
 
 // Interface for the C binding
 #[no_mangle]
-pub extern fn calc(input: *const c_uchar, input_len: c_int,  par1: c_short, par2: c_short, trans: *const c_uchar) -> *mut c_char 
+pub extern fn calc(path: *const c_char, input_len: c_int,  par1: c_short, par2: c_short, trans: *const c_uchar) -> *mut c_char 
 {
-    // Some memory leaks are possible 
-    // let c_str_path = unsafe { CStr::from_ptr(path) };
-    // let rust_str_path = c_str_path.to_str().unwrap().to_string();
+    // Some memory leaks are possible ¯\_(ツ)_/¯
+    let c_str_path = unsafe { CStr::from_ptr(path) };
+    let rust_str_path = c_str_path.to_str().unwrap().to_string();
     
     let trans2;
-    let input2;
+    // let input2;
 
     // ¯\_(ツ)_/¯
     unsafe{
@@ -24,11 +26,11 @@ pub extern fn calc(input: *const c_uchar, input_len: c_int,  par1: c_short, par2
         let d3 = d2.cloned();
         trans2 = d3;
 
-        let d4 = make_slice(input, input_len as usize); // Cast works ONLY for 64-bit platforms! https://stackoverflow.com/a/50437859/15776812
-        input2 = d4;
+        // let d4 = make_slice(input, input_len as usize); // Cast works ONLY for 64-bit platforms! https://stackoverflow.com/a/50437859/15776812
+        // input2 = d4;
     }
 
-    let r = match calc_inner(input2, par1, par2, trans2) {
+    let r = match calc_inner(rust_str_path, par1, par2, trans2) {
         Ok(h) => h,
         Err(_e) => "Error".to_string(),
     };
@@ -46,16 +48,16 @@ pub extern fn version() -> *mut c_char {
     return CString::new(r).unwrap().into_raw();
 }
 
-pub fn calc_inner(input: &[u8], par1: i16, par2: i16, trans: Option<[u8;4]>)->Result<String, P3DError>{
-    // let f = File::open(path)?;
-    // let mut input = BufReader::new(f);
-    // let mut buf = Vec::new();
+pub fn calc_inner(path: String, par1: i16, par2: i16, trans: Option<[u8;4]>)->Result<String, std::io::Error>{
+    let f = File::open(path)?;
+    let mut input = BufReader::new(f);
+    let mut buf = Vec::new();
 
-    // input.read_to_end(&mut buf);
+    input.read_to_end(&mut buf);
 
-    // let c: &[u8] = &buf;
+    let c: &[u8] = &buf;
     
-    let res_hashes = p3d_process(input, AlgoType::Grid2d, par1, par2, trans);
+    let res_hashes = p3d_process(c, AlgoType::Grid2d, par1, par2, trans);
 
     let r = match res_hashes {
         Ok(h) => h,
